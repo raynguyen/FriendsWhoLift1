30d3059d4f3946068456b2347d9a5a05f1d5ff1e
ray_laptop <raynguyen64@gmail.com>
2019-01-17 20:29:15 -0800

    01/17/2019
    -Attached default ActionBar to TestActivity.
    --Created a Menu xml file (home_actionbar).

--- app/src/main/java/apps/raymond/friendswholift/TestActivity.java
+++ app/src/main/java/apps/raymond/friendswholift/TestActivity.java
@@ -2,6 +2,8 @@ package apps.raymond.friendswholift;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
+
+        Toolbar top_toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(top_toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.home_actionbar, menu);
+        return true;
     }
 }
