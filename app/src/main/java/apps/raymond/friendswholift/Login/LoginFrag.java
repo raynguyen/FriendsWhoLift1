6eea779bb04f86540722edc4e64e148102a09652
ray_laptop <raynguyen64@gmail.com>
2019-01-20 11:47:39 -0800

    01/20/2019
    Added a PagerAdapter and various Fragments for the LoginActivity.

---
+++ app/src/main/java/apps/raymond/friendswholift/Login/LoginFrag.java
@@ -0,0 +1,22 @@
+package apps.raymond.friendswholift.Login;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import apps.raymond.friendswholift.R;
+
+public class LoginFrag extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState){
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.login_frag, container, false);
+    }
+}
