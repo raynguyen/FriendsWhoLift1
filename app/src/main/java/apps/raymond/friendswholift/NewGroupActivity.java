4aee8c8dfba30b92388864feaa85a69e88bee9b9
ray <raynguyen64@gmail.com>
2019-01-27 12:23:17 -0800

    1/27/2019
    Working on the Create a group activity.
    Currently working on adding Documents to a Collection.

--- app/src/main/java/apps/raymond/friendswholift/NewGroupActivity.java
+++ app/src/main/java/apps/raymond/friendswholift/NewGroupActivity.java
@@ -1,16 +1,46 @@
+/*
+ * When the back button is pressed, we want to save the current state of the activity. Upon resume,
+ * should load the previous state of the activity prior to the back press.
+ */
 package apps.raymond.friendswholift;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class NewGroupActivity extends AppCompatActivity {
+public class NewGroupActivity extends AppCompatActivity implements
+        AdapterView.OnItemSelectedListener {
     private static final String TAG = "NewGroupActivity";
+
+    private Spinner invite_Spinner;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_group);
 
+        invite_Spinner = findViewById(R.id.invite_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter
+                .createFromResource(NewGroupActivity.this, R.array.array_invite_authorize,
+                        android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        invite_Spinner.setAdapter(adapter);
+    }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Log.i(TAG,"Clicked on a spinner item.");
+
+        Toast.makeText(this,"Clicked on a spinner item",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
     }
 }
