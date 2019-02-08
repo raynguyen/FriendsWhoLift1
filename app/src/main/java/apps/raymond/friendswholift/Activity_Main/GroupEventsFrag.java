9b42ad111701ae90701a61fe564b2e7f31da4715
ray <raynguyen64@gmail.com>
2019-02-07 19:47:14 -0800

    2/7/2019
    Work on collecting the approriate POJO classes from the Group Collection to build our RecyclerView's CardViews of the upcoming events.

--- app/src/main/java/apps/raymond/friendswholift/Activity_Main/GroupEventsFrag.java
+++ app/src/main/java/apps/raymond/friendswholift/Activity_Main/GroupEventsFrag.java
@@ -17,6 +17,7 @@ import android.widget.ImageButton;
 
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
@@ -109,14 +110,15 @@ public class GroupEventsFrag extends Fragment implements EventClickListener, Vie
     @Override
     public void onClick(View v) {
         int i = v.getId();
+        final FirebaseUser currUser = FirebaseAuth.getInstance().getCurrentUser();
+        final List<String> myEvents = new ArrayList<>();
+        final Task<List<Task<DocumentSnapshot>>> myGroupEvents;
+
         switch (i){
-            case R.id.create_event:
-
-                final List<String> myEvents = new ArrayList<>();
-                Task<List<Task<DocumentSnapshot>>> myGroupEvents;
-
+            case R.id.create_event://create_event
                 //Test method to see if we can return a List of the Document names from the Events Collection **WORKS
-                final FirebaseUser currUser = FirebaseAuth.getInstance().getCurrentUser();
+
+
                 myGroupEvents = FirebaseFirestore.getInstance().collection("Users").document(currUser.getEmail()).collection("Events").get()
                         .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                             @Override
@@ -124,25 +126,31 @@ public class GroupEventsFrag extends Fragment implements EventClickListener, Vie
                                 if(task.isSuccessful()){
                                      for(QueryDocumentSnapshot event:task.getResult()){
                                          myEvents.add(event.getId());
-                                         Log.i(TAG, myEvents.toString());
                                      }
+                                     Log.i(TAG,"These are stored in the events: " + myEvents.toString());
                                 }
                             }
+                            //The above will get us a list of the Event names in the User's Event Collection.
+                            //Use this list to retrieve the appropriate POJO document from our Groups Collection.
+
+                            
                             //This continuation will try to collect all the GroupEvent objects from the
                         }).continueWith(new Continuation<QuerySnapshot, List<Task<DocumentSnapshot>>>() {
-                    @Override
-                    public List<Task<DocumentSnapshot>> then(@NonNull Task<QuerySnapshot> task) throws Exception {
-                        Log.i(TAG,"Continuing to get the GroupEvents ");
-                        final List<Task<DocumentSnapshot>> myGroupEvents;
-                        myGroupEvents = new ArrayList<>();
-                        for(String eventName : myEvents){
-                            Log.i(TAG,"Attempting to retrieve GroupEvent POJO named: "+eventName);
-                            myGroupEvents.add(FirebaseFirestore.getInstance().collection("Groups").document(currUser.getEmail()).collection("Events").document(eventName).get());
-                        }
-                        return myGroupEvents;
-                    }
-                });
+                            @Override
+                            public List<Task<DocumentSnapshot>> then(@NonNull Task<QuerySnapshot> task) throws Exception {
+                                Log.i(TAG,"Continuing to get the GroupEvents ");
+                                final List<Task<DocumentSnapshot>> myGroupEvents;
+                                myGroupEvents = new ArrayList<>();
+                                for(String eventName : myEvents){
+                                    Log.i(TAG,"Attempting to retrieve GroupEvent POJO named: "+eventName);
+                                    //myGroupEvents.add(FirebaseFirestore.getInstance().collection("Groups").document(currUser.getEmail()).collection("Events").document(eventName).get());
+                                }
+                                myGroupEvents.add(FirebaseFirestore.getInstance().collection("Users").document(currUser.getEmail()).get());
+                                return myGroupEvents;
+                            }
+                        });
 
+                /*
                 // From this we can conclude that myGroupEvents returns a List of tasks to retrieve documents.
                 myGroupEvents.addOnCompleteListener(new OnCompleteListener<List<Task<DocumentSnapshot>>>() {
                     @Override
@@ -161,7 +169,7 @@ public class GroupEventsFrag extends Fragment implements EventClickListener, Vie
                         });
                     }
                 });
-
+                */
 
 
 
@@ -173,6 +181,25 @@ public class GroupEventsFrag extends Fragment implements EventClickListener, Vie
                         .add(R.id.event_FrameLayout,createEventFragment)
                         .show(createEventFragment)
                         .commit();*/
+                break;
+            case R.id.cancel_btn: // This method shows that we are unable to continue if the Document reference does not exist.
+                FirebaseFirestore.getInstance().collection("Users").document("WHACKATTACK").collection("Events").get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if(task.isSuccessful()){
+                                    for(QueryDocumentSnapshot event:task.getResult()){
+                                        Log.i(TAG,"APPARENTLY THERES A WHACKATTACK DOC?>");
+                                    }
+                                }
+                            }
+                            //This continuation will try to collect all the GroupEvent objects from the
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG,"Unable to retrieve the WHACKATTACK DOC",e);
+                    }
+                });
                 break;
         }
     }
