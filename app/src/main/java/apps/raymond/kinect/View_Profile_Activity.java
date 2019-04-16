c634264017ed6f1069226cf313b9c45ba0ac7d17
ray_laptop <raynguyen64@gmail.com>
2019-04-15 19:49:50 -0700

    View Users Activities and Fragments

--- app/src/main/java/apps/raymond/kinect/View_Profile_Activity.java
+++ app/src/main/java/apps/raymond/kinect/View_Profile_Activity.java
@@ -2,24 +2,53 @@ package apps.raymond.kinect;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import apps.raymond.kinect.UserProfile.UserModel;
 
-public class View_Profile_Activity extends FragmentActivity {
+public class View_Profile_Activity extends AppCompatActivity {
     private static final String TAG ="ViewProfile";
     public static final String USER = "UserModel";
 
+    UserModel user;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_profile_activity);
         Bundle args = getIntent().getExtras();
 
-        UserModel userModel = args.getParcelable(USER);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        Log.i(TAG,"Starting view profile activity for user: " + userModel.getEmail());
+        user = args.getParcelable(USER);
+
+        Log.i(TAG,"Starting view profile activity for user: " + user.getEmail());
     }
 
+    MenuItem addUserAction, msgUserAction;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu,menu);
+        addUserAction = menu.findItem(R.id.action_connect);
+        msgUserAction = menu.findItem(R.id.action_message);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_connect:
+                Log.i(TAG,"Connect with user.");
+                return true;
+            case R.id.action_message:
+                Log.i(TAG,"Message user.");
+                return true;
+            default:
+                return false;
+        }
+    }
 }
