a7bfc12665cc9dcf85cb825da36d8dc37e88f48b
ray_laptop <raynguyen64@gmail.com>
2019-07-20 11:10:05 -0700

    Event Create Fragment

--- app/src/main/java/apps/raymond/kinect/CoreFragments/Create_Fragment.java
+++ app/src/main/java/apps/raymond/kinect/CoreFragments/Create_Fragment.java
@@ -5,14 +5,19 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
+import apps.raymond.kinect.ProfileRecyclerAdapter;
 import apps.raymond.kinect.R;
+import apps.raymond.kinect.UserProfile.User_Model;
 import apps.raymond.kinect.ViewModels.Core_ViewModel;
 
-public class Create_Fragment extends Fragment {
+public class Create_Fragment extends Fragment implements ProfileRecyclerAdapter.ProfileClickListener {
     private Core_ViewModel mViewModel;
 
     @Override
@@ -30,5 +35,23 @@ public class Create_Fragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        List<User_Model> myConnections = mViewModel.getUserConnections().getValue();
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_create_invite);
+        ProfileRecyclerAdapter adapter = new ProfileRecyclerAdapter(this);
+
+        if(myConnections == null){
+            //mViewModel.lo
+        }
+    }
+
+    @Override
+    public void onProfileClick(User_Model userModel) {
+
+    }
+
+    @Override
+    public void onProfileLongClick(User_Model userModel) {
+
     }
 }
