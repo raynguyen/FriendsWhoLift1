ce69c8894583e14820765bf31981b57b309601a5
ray_laptop <raynguyen64@gmail.com>
2019-08-08 12:07:25 -0700

    AndroidX migration

--- app/src/main/java/apps/raymond/kinect/Interfaces/LocationsListenerInterface.java
+++ app/src/main/java/apps/raymond/kinect/Interfaces/LocationsListenerInterface.java
@@ -8,3 +8,11 @@ public interface LocationsListenerInterface {
     void setLocations(List<Location_Model> locations);
     void onLocationClick(Location_Model location);
 }
+
+/*
+ToDo:
+ Need to determine a pattern that allows the LocationsRecycler_Fragment to update observe a specific
+ ViewModel without directly requiring the Fragment class to distinguish between which ViewModel is
+ being observed (i.e. we want the parent activity to observe a ViewModel and cascade changes in the
+ Locations data set to any children LocationsRecycler_Fragments.
+ */
